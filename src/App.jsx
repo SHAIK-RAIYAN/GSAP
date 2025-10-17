9b92bfb9a24354576f39662b2a235e31c81fd2ca
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-10-17 23:41:47 +0530

    Learning GSAP

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CircleAnimate from "./components/CircleAnimate";
+import FiveScrollTrigger from "./components/FiveScrollTrigger";
+import FourNavBar from "./components/FourNavBar";
+import SixTextSlide from "./components/SixTextSlide";
+import Three from "./components/Three";
+import TwoboxesAnimate from "./components/TwoboxesAnimate";
+function App() {
+  return (
+    <main className=" bg-[#111] text-white h-screen">
+      
+    </main>
+  );
+}
+
+export default App;
