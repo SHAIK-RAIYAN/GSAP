05e91801035e19d51a06b9c5e3ad9a07c8718a06
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-10-18 17:25:34 +0530

    yoyo square

---
+++ src/components/Three.jsx
@@ -0,0 +1,35 @@
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { gsap } from "gsap";
+
+function Three() {
+  const square = useRef();
+
+  useGSAP(() => {
+    gsap.fromTo(
+      square.current,
+      { x: 100 },
+      {
+        rotate: 360,
+        x: 500,
+        backgroundColor: "#38bdf8", //if want to change color then it should not be any tailwind
+        // use inline or normal css
+        duration: 2,
+        repeat: -1,
+        delay: 1,
+        yoyo: true,
+      }
+    );
+  });
+
+  return (
+    <div
+      ref={square}
+      className="size-50 flex justify-center items-center font-bold text-2xl"
+      style={{ backgroundColor: "#fb7185" }}>
+      square
+    </div>
+  );
+}
+
+export default Three;
