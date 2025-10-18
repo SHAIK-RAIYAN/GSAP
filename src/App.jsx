73037ebc56c3b78f3bc81b89426499a6831e50d9
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-10-18 17:26:32 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import CircleAnimate from "./components/CircleAnimate";
 import FiveScrollTrigger from "./components/FiveScrollTrigger";
 import FourNavBar from "./components/FourNavBar";
+import SevenString from "./components/SevenString";
 import SixTextSlide from "./components/SixTextSlide";
 import Three from "./components/Three";
 import TwoboxesAnimate from "./components/TwoboxesAnimate";
 function App() {
   return (
     <main className=" bg-[#111] text-white h-screen">
+      {/* <TwoboxesAnimate /> */}
+      {/* <CircleAnimate /> */}
+      {/* <Three/> */}
+      {/* <FourNavBar /> */}
       
     </main>
   );
