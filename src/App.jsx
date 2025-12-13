e142a6c1cea0b9343f6719fdd2775260ecb5a920
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-12-13 21:03:43 +0530

    added lenis scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,60 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Lenis from "lenis";
 import { useRef } from "react";
+import { Route, Routes } from "react-router-dom";
 import CircleAnimate from "./components/CircleAnimate";
 import EightCustomCursor from "./components/EightCustomCursor";
+import ElevenSideBar from "./components/ElevenSideBar";
 import FiveScrollTrigger from "./components/FiveScrollTrigger";
 import FourNavBar from "./components/FourNavBar";
+import NineCursorChange from "./components/NineCursorChange";
 import SevenString from "./components/SevenString";
 import SixTextSlide from "./components/SixTextSlide";
-import Three from "./components/Three";
-import TwoboxesAnimate from "./components/TwoboxesAnimate";
-import NineCursorChange from "./components/NineCursorChange";
 import TenMaskCursor from "./components/TenMaskCursor";
-import ElevenSideBar from "./components/ElevenSideBar";
+import ThirteenScrollingText from "./components/ThirteenScrollingText";
+import ThreeYo_yo from "./components/ThreeYo_yo";
 import TwelveTextAppear from "./components/TwelveTextAppear";
-import { Route, Routes } from "react-router-dom";
+import TwoboxesAnimate from "./components/TwoboxesAnimate";
 import HomePage from "./pages/HomePage";
 
 function App() {
   const mainRef = useRef();
 
+  const lenis = new Lenis();
+  lenis.on("scroll", ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+  gsap.ticker.lagSmoothing(0);
   return (
-    <main ref={mainRef} className=" bg-[#111] text-white min-h-screen">
+    <main
+      ref={mainRef}
+      className=" bg-[#111] text-white min-h-screen scroll-smooth">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/two-boxes" element={<TwoboxesAnimate />} />
         <Route path="/circle-animate" element={<CircleAnimate />} />
-        <Route path="/three-yoyo" element={<Three />} />
-        <Route path="/four-nav-bar" element={<FourNavBar />} />
-        <Route path="/five-scroll-trigger" element={<FiveScrollTrigger />} />
-        <Route path="/six-text-slide" element={<SixTextSlide />} />
-        <Route path="/seven-string" element={<SevenString />} />
+        <Route path="/yoyo" element={<ThreeYo_yo />} />
+        <Route path="/nav-bar" element={<FourNavBar />} />
+        <Route path="/scroll-trigger" element={<FiveScrollTrigger />} />
+        <Route path="/text-slide" element={<SixTextSlide />} />
+        <Route path="/string" element={<SevenString />} />
         <Route
-          path="/eight-custom-cursor"
+          path="/custom-cursor"
           element={<EightCustomCursor mainRef={mainRef} />}
         />
         <Route
-          path="/nine-cursor-change"
+          path="/cursor-change"
           element={<NineCursorChange mainRef={mainRef} />}
         />
         <Route
-          path="/ten-mask-cursor"
+          path="/mask-cursor"
           element={<TenMaskCursor mainRef={mainRef} />}
         />
-        <Route path="/eleven-side-bar" element={<ElevenSideBar />} />
-        <Route path="/twelve-text-appear" element={<TwelveTextAppear />} />
+        <Route path="/side-bar" element={<ElevenSideBar />} />
+        <Route path="/text-appear" element={<TwelveTextAppear />} />
+        <Route path="/scrolling-text" element={<ThirteenScrollingText />} />
       </Routes>
     </main>
   );
