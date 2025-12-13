891a59f652517cd7a7fd1c27963eb40cd4732d79
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-12-13 20:55:12 +0530

    shifted cards from homepage to data folder

---
+++ src/data/cards.js
@@ -0,0 +1,82 @@
+const cards = [
+  {
+    name: "Boxes Animate",
+    image: "/bg-image.jpg",
+    path: "/two-boxes",
+    desc: "Simple 2 boxes animation.",
+  },
+  {
+    name: "Circle Animate",
+    image: "/bg-image.jpg",
+    path: "/circle-animate",
+    desc: "Click to animate Circle position.",
+  },
+  {
+    name: "Color and Yo-yo",
+    image: "/bg-image.jpg",
+    path: "/yoyo",
+    desc: "Color Changing and yo-yo effects.",
+  },
+  {
+    name: "Nav Bar",
+    image: "/bg-image.jpg",
+    path: "/nav-bar",
+    desc: "A animated navigation menu.",
+  },
+  {
+    name: "Scroll Trigger",
+    image: "/bg-image.jpg",
+    path: "/scroll-trigger",
+    desc: "Animating elements on scroll .",
+  },
+  {
+    name: "Text Slide",
+    image: "/bg-image.jpg",
+    path: "/text-slide",
+    desc: "Scroll page to text sliding effects.",
+  },
+  {
+    name: "Hover String animate",
+    image: "/bg-image.jpg",
+    path: "/string",
+    desc: "SVG string and path animations.",
+  },
+  {
+    name: "Custom Cursor",
+    image: "/bg-image.jpg",
+    path: "/custom-cursor",
+    desc: "A custom cursor that interacts with elements.",
+  },
+  {
+    name: "Cursor Change",
+    image: "/bg-image.jpg",
+    path: "/cursor-change",
+    desc: "Changing cursor style on hover.",
+  },
+  {
+    name: "Mask Cursor",
+    image: "/bg-image.jpg",
+    path: "/mask-cursor",
+    desc: "Using the cursor to reveal content.",
+  },
+  {
+    name: "Side Bar",
+    image: "/bg-image.jpg",
+    path: "/side-bar",
+    desc: "A animated slide-out sidebar.",
+  },
+  {
+    name: "Text Appear",
+    image: "/bg-image.jpg",
+    path: "/text-appear",
+    desc: "Staggered text character appearance.",
+  },
+  {
+    name: "Scrolling Text",
+    image: "/bg-image.jpg",
+    path: "/scrolling-text",
+    desc: "Horizanlly moving text on scroll",
+  },
+];
+
+export default cards;
