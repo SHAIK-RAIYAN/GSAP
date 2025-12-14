0fbe4f203a252a1d47752917a0188185ca9bf913
SHAIK RAIYAN <shaikraiyan2005@gmail.com>
2025-12-14 05:38:16 +0530

    added stairs as <App /> parent

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Stairs from "./components/common/Stairs.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Stairs>
+        <App />
+      </Stairs>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
